6a8bfb9c898eb2fd0b78a7ab30b9ce045c66d3fc
Fall Xavier <84975017+Fall-Xavier@users.noreply.github.com>
2024-11-17 12:22:52 +0700

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -15,10 +15,53 @@ def add_header(response):
     
 @bp.route("/latest/")
 def latest():
-    page = request.args.get("page", 1, type=int)
     try:
-        data = Komik(f"komik-terbaru/page/{page}/","latest").main()
+        data = Komik("").latest()
         return Result("success", data, 200)
     except Exception as e:
         return Result(f"error {e}", {"data": None}, 400)
         
+@bp.route("/populer/")
+def populer():
+    try:
+        data = Komik("manga/?status=&order=popular").search()
+        return Result("success", data, 200)
+    except Exception as e:
+        return Result(f"error {e}", {"data": None}, 400)
+        
+@bp.route("/genre-list/")
+def genrelist():
+    try:
+        data = Komik("manga/?status=&order=popular").genrelist()
+        return Result("success", data, 200)
+    except Exception as e:
+        return Result(f"error {e}", {"data": None}, 400)
+        
+@bp.route("/search/")
+def search():
+    genre = request.args.get("genre", "")
+    status = request.args.get("status", "")
+    type = request.args.get("type", "")
+    order = request.args.get("order", "")
+    page = request.args.get("page", 1, type=int)
+    try:
+        data = Komik(f"manga/?page={page}&genre%5B%5D={genre}&status={status}&type={type}&order={order}").search()
+        return Result("success", data, 200)
+    except Exception as e:
+        return Result(f"error {e}", {"data": None}, 400)
+        
+@bp.route("/komik/<slug>/")
+def detail(slug):
+    try:
+        data = Komik(f"https://komikstation.co/manga/{slug}").detail()
+        return Result("success", data, 200)
+    except Exception as e:
+        return Result(f"error {e}", {"data": None}, 400)
+        
+@bp.route("/chapter/<slug>/")
+def viewchapter(slug):
+    try:
+        data = Komik(f"https://komikstation.co/{slug}").viewchapter()
+        return Result("success", data, 200)
+    except Exception as e:
+        return Result(f"error {e}", {"data": None}, 400)
