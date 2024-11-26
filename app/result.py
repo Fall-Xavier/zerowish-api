17885669d1adc81706c7c1685c92c4a5ea85106b
Fall Xavier <84975017+Fall-Xavier@users.noreply.github.com>
2024-11-27 01:01:20 +0700

    Update result.py

--- app/result.py
+++ app/result.py
@@ -2,12 +2,6 @@ from flask import jsonify
 
 def Result(status,data,status_code):
     return (
-        jsonify(
-            {
-                "author": "fajrul falah",
-                "status": status,
-                "data": data,
-            }
-        ),
+        jsonify(data),
         status_code,
     )
